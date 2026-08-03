a40a739928712d48685cd00abd371d09746b220b
unknown <letanthanh160606@gmail.com>
2026-08-03 23:57:13 +0700

    fix(models): clean import conflicts

--- src/backend/app/models.py
+++ src/backend/app/models.py
@@ -1,13 +1,8 @@
 import uuid
 from datetime import datetime
 
-<<<<<<< HEAD
-from sqlalchemy import Boolean, DateTime, Enum, ForeignKey, String, Text, func, text
+from sqlalchemy import Boolean, DateTime, Enum, ForeignKey, SmallInteger, String, Text, func, text
 from sqlalchemy.dialects.postgresql import JSONB, UUID
-=======
-from sqlalchemy import Boolean, DateTime, Enum, ForeignKey, SmallInteger, String, func, text
-from sqlalchemy.dialects.postgresql import UUID
->>>>>>> main
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
